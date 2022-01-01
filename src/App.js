9ade84baa8a67d6101597985d23a25183957e26e
faozziyyah <omowunmidaud1@gmail.com>
2022-01-01 15:57:33 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Countries from './components/Countries';
+import Filter from './components/Filter';
 import Header from './components/Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>   
         <Header />
+        <Filter />
         <Countries />  
     </>
   );
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,7 +3,22 @@ import React from 'react'
 const Filter = () => {
     return (
         <div>
-            
+            <section className="filter">
+                <form className="form-control">
+                    <input type="search" name="search" id="search" placeholder="Search for a country" />
+                </form>
+
+                <div className="region-filter">
+                    <select name="select" id="select" className="select">
+                        <option value="filter by region">Filter by region</option>
+                        <option value="Africa">Africa</option>
+                        <option value="America">America</option>
+                        <option value="Asia">Asia</option>
+                        <option value="Europia">Europia</option>
+                        <option value="Oceonia">Oceonia</option>
+                    </select>
+                </div>
+            </section>
         </div>
     )
 }
